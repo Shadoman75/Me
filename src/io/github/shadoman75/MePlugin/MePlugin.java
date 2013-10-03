e454e3f29209bb5d2a5f19d7d14e069783e5b4e6
shadoman75 <shadowkiller77776@yahoo.com>
2013-10-03 18:51:03 -0500

    Updates!

--- src/io/github/shadoman75/MePlugin/MePlugin.java
+++ src/io/github/shadoman75/MePlugin/MePlugin.java
@@ -2,16 +2,25 @@ package io.github.shadoman75.MePlugin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
 
 public final class MePlugin extends JavaPlugin {
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		// if someone just happened to type /me and have a message after it...
+	public void onDisable(){
+		getLogger().info("Disabled... for now");
+	}
+	public void onEnable(){
+		getLogger().info("Enabled hopefully");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd,
+		String commandLabel, String[] args); {
+		Player player = (Player) sender;
+		// if someone just happened to type /me and wanted to have a message after it...
 		if(cmd.getName().equalsIgnoreCase("me")){
 			
-			
+			sendMessage();
 		}
 	}
 
